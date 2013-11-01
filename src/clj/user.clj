15f95bc0511102bc319b4588321f910202bb0c33
Francis <franciswolke@gmail.com>
2013-11-01 06:34:10 -0700

    Implemented intial version of smart select

--- src/clj/user.clj
+++ src/clj/user.clj
@@ -7,33 +7,6 @@
             [cemerick.pomegranate :refer (add-dependencies)]
             [clojure.reflect :as r]))
 
-;; (defn ss
-;;   "[S]mart [s]elect. Given a start map and set of goal-keys, generates a function
-;; to convert one to the other (start to goal). EG:
-;; ... call ...
-;; ;=> (defn foo [in out] {:key1 (get-in in [:foo :bar]) ...})
-    
-
-;;  If multiple paths exist to any of the 
-;; goal keys, a hashmap is returned {:duplicate-key [[:path :to :key] [:other :path] ...]} keyed by 
-;; duplicates with the "
-;;   ([start goal-keys]
-;;      (loop [goal-keys]))
-;;   ([start goal-keys path-map]))
-
-(defn m->ds
-  "Accepts a hashmap, which is then converted into its destructuring syntax."
-  [m]
-  (loop [ks (keys m)
-         acc {}]
-    (if (empty? ks)
-      acc
-      (recur (rest ks)
-             (let [fk (first ks)
-                   v (get m fk)
-                   sfk (symbol (apply str (rest (str fk))))]
-               (assoc acc (if (map? v) (m->ds v) sfk) fk))))))
-
 (defn c= [n body]
   (= n (count body)))
 
@@ -60,3 +33,65 @@
    :coordinates `[~coordinate]
    :repositories (merge cemerick.pomegranate.aether/maven-central
                         {"clojars" "http://clojars.org/repo"})))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; Hackage
+
+(defn m->ds
+  "Accepts a hashmap, returns it's destructuring syntax."
+  [m]
+  (loop [ks (keys m)
+         acc {}]
+    (if (empty? ks)
+      acc
+      (recur (rest ks)
+             (let [fk (first ks)
+                   v (get m fk)
+                   sfk (symbol (apply str (rest (str fk))))]
+               (assoc acc (if (map? v) (m->ds v) sfk) fk))))))
+
+(defn paths-to
+  "Returns all possible paths to (k)ey within a given (m)ap"
+  [k m & context]
+  (letfn [(f [[kk mm]] (if context
+                         (apply paths-to (cons k (cons mm (cons kk context))))
+                         (paths-to k mm kk)))
+          (m? [[_ b]] (map? b))]
+    (->> (cond (and (m k) (some map? (vals m))) (reduce into [(if context (cons k context) `(~k))
+                                                              (map f (filter m? m))])
+               (m k) (if context (cons k context) `(~k))
+               (some map? (vals m)) (reduce into (map f (filter m? m)))
+               :else ())
+         (filter #(or (keyword? %) (not (empty? %))))
+         (map #(if (coll? %) (reverse %) %)))))
+
+(defn ss
+  ;; TODO, Fri Nov 01 2013, Francis Wolke
+  ;; Dosn't obey spec.
+  "[s]mart [s]elect. 
+Given a start map and `seq' of goal-keys, generates a function to convert one to the other (start to goal). EG:
+... call ...
+;=> (fn [in out] {:key1 (get-in in [:foo :bar]) ...})
+
+ If multiple paths exist to any of the 
+goal keys, a hashmap is returned keyed by duplicates where the value is all possible
+paths to the duplicate key.
+
+{:duplicate-key [[:path :to :key] [:other :path] ...]}"
+  ([start goal-keys]
+     (loop [goal-keys goal-keys
+            duplicate-keys {}
+            out {}]
+       (cond (and (empty? goal-keys) (empty? duplicate-keys)) out
+             (empty? goal-keys) duplicate-keys
+             :else (let [fst (first goal-keys)
+                         paths (paths-to fst start)]
+                     (if (= 1 (count paths))
+
+                       (recur (rest goal-keys)
+                              duplicate-keys 
+                              (assoc out fst paths))
+
+                       (recur (rest goal-keys)
+                              (assoc duplicate-keys fst paths)
+                              out)))))))
