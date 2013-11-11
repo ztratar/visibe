4d87aa5f9c328d0c089af701c8b05fdb681492fd
Francis <franciswolke@gmail.com>
2013-11-10 16:46:11 -0800

    Added LESS script in HTML we are serving from visibe

--- src/clj/visibe/core.clj
+++ src/clj/visibe/core.clj
@@ -21,11 +21,26 @@
           [:title "Visibe - Watch situations and reactions unfold as they happen"]
           [:meta {:charset "UTF-8"}]
           [:link {:rel "stylesheet/less" :type "text/css" :href "css/styles.less"}]]
-          [:body (include-js "js/libs/d3.v3.min.js"
-                            "https://apis.google.com/js/plusone.js"
-                            "js/visibe_dbg.js" "js/libs/video-js/video.js"
-                            "js/libs/less.js"
-                            "js/libs/moment-with-langs.js")
+         [:body [:script {:type "text/javascript"}
+                 "less = {
+        env: \"development\", // or \"production\"
+        async: false,       // load imports async
+        fileAsync: false,   // load imports async when in a page under
+                            // a file protocol
+        poll: 1000,         // when in watch mode, time in ms between polls
+        functions: {},      // user functions, keyed by name
+        dumpLineNumbers: \"comments\", // or \"mediaQuery\" or \"all\"
+        relativeUrls: false,// whether to adjust url's to be relative
+                            // if false, url's are already relative to the
+                            // entry less file
+        rootpath: \":/a.com/\"// a path to add on to the start of every url
+                            //resource
+};"]
+          (include-js "js/libs/d3.v3.min.js"
+                      "https://apis.google.com/js/plusone.js"
+                      "js/visibe_dbg.js" "js/libs/video-js/video.js"
+                      "js/libs/less.js"
+                      "js/libs/moment-with-langs.js")
           [:div#content]]))
 
 (defroutes app-routes
