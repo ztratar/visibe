e2e18afe4da6cb998b8e8c22f75c3a150ddb5651
Francis <franciswolke@gmail.com>
2013-11-19 14:59:02 -0800

    Removed extra code from visibe.feeds.flickr

--- src/clj/visibe/feeds/flickr.clj
+++ src/clj/visibe/feeds/flickr.clj
@@ -12,7 +12,7 @@ Relevent page: http://www.flickr.com/services/api/misc.urls.html"}
            javax.imageio.ImageIO))
 
 (defn buffered-images-equal?
-  "Checks for byte equality against 'PROJECT_ROOT/resources/public/flickr-not-found.jpg'"
+  "Checks for equality with 'PROJECT_ROOT/resources/public/flickr-not-found.jpg'"
   [image-1 image-2]
   (let [i1-width (.getWidth image-1)
         i1-height (.getHeight image-1)
@@ -21,21 +21,7 @@ Relevent page: http://www.flickr.com/services/api/misc.urls.html"}
         i2-height  (.getHeight image-2)]
 
     (and (= i1-width i2-width)
-         (= i1-height i2-height)
-         ;; TODO, XXX Tue Nov 05 2013, Francis Wolke
-         ;; This code is buggy, I can't tell what's going wrong with it and
-         ;; don't feel like spending any more time on it right now. It'll work
-         ;; unless we happen to get an image with the exact same dimensions as
-         ;; 'flickr-image-not-found.jpg'. In that case, we will have filtered
-         ;; out an image that we shouldn't have. ¯\_(:/)_/¯
-
-         ;; (loop [coords [0 0]]
-         ;;     (let [[x y] coords]
-         ;;       (cond (= [x y] [i2-width i2-height]) true
-         ;;             (= (.getRGB image-1 x y)
-         ;;                (.getRGB image-2 x y)) (recur (if (= x i1-width) [0 (inc y)] [(inc x) y]))
-         ;;             :else false)))
-         )))
+         (= i1-height i2-height))))
 
 (defn too-small? [img]
   (or (>= 300 (.getWidth img))
@@ -49,9 +35,6 @@ Relevent page: http://www.flickr.com/services/api/misc.urls.html"}
     url))
 
 (defn trend->photo-url
-  "Returns 10 URLs for associated with a trend"
-  ;; TODO, Fri Oct 18 2013, Francis Wolke
-  ;; This will pull back images that are too small (or large) for our needs. There needs to be 'intelligence' to pick suitable images.
   [trend]
   (let [req-body (:body (client/get (str "http://api.flickr.com/services/rest/?method=flickr.photos.search&api_key="
                                          (gis [:flickr :key]) "&text=" (url-encode trend))))
